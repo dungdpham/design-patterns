adc406c6e85ba73df642261eec3b2939d5bff156
dungdpham <dung.pham3@metropolia.fi>
2024-03-08 02:17:17 +0200

    add assignment 7 and 8

---
+++ state/src/Level.java
@@ -0,0 +1,19 @@
+public abstract class Level {
+    private Character character;
+    protected final int EXP_TRAIN = 5;
+    protected final int EXP_FIGHT = 10;
+    protected final int HP_MEDITATE = 5;
+    protected final int HP_FIGHT = 10;
+
+    public Level(Character character) {
+        this.character = character;
+    }
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    abstract String getLevel();
+
+    abstract void action();
+}
---
+++ state/src/MasterLevel.java
@@ -0,0 +1,16 @@
+public class MasterLevel extends Level {
+    public MasterLevel(Character character) {
+        super(character);
+    }
+
+    @Override
+    public String getLevel() {
+        return "Master";
+    }
+
+    @Override
+    public void action() {
+        System.out.println("You are a Master. Return to main menu.\n");
+        this.getCharacter().setIsPlaying(false);
+    }
+}
